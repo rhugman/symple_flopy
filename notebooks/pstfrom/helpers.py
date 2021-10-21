c166a96009abd003a42a2539c7edc04474800b94
unknown <jtwhite1000@gmail.com>
2021-10-21 17:08:23 -0600

    patched pstfrom for 2 sp model

--- notebooks/pstfrom/helpers.py
+++ notebooks/pstfrom/helpers.py
@@ -33,18 +33,20 @@ def postproc():
 
 	ucn = [f for f in os.listdir(".") if f.lower().endswith(".ucn")][0]
 	ucn = flopy.utils.HeadFile(ucn,precision="double",text="concentration")
-	d = ucn.get_data()
-	for k,dlay in enumerate(d):
-		np.savetxt("sgn_50.lay{0}_ucn.dat".format(k+1),d[k,:,:],fmt="%15.6E")
+	for it,t in enumerate(ucn.get_times()):
+		d = ucn.get_data(totim=t)
+		for k,dlay in enumerate(d):
+			np.savetxt("sgn_50.lay{0}_t{1}_ucn.dat".format(k+1,it+1),d[k,:,:],fmt="%15.6E")
 	hds = [f for f in os.listdir(".") if f.lower().endswith(".hds")][0]
 	hds = flopy.utils.HeadFile(hds)
-	d = hds.get_data()
-	for k,dlay in enumerate(d):
-		np.savetxt("sgn_50.lay{0}_hds.dat".format(k+1),d[k,:,:],fmt="%15.6E")
+	for it,t in enumerate(hds.get_times()):
+		d = hds.get_data(totim=t)
+		for k,dlay in enumerate(d):
+			np.savetxt("sgn_50.lay{0}__t{1}_hds.dat".format(k+1,it+1),d[k,:,:],fmt="%15.6E")
 
 	lst = [f for f in os.listdir(".") if f.lower().endswith(".list")][0]
 	lst = flopy.utils.Mf6ListBudget(lst)
-	cum,inc = lst.get_dataframes(diff=True,start_datetime=None)
+	inc,cum = lst.get_dataframes(diff=True,start_datetime=None)
 	cum.columns = cum.columns.map(lambda x: x.lower().replace(" ","-"))
 	inc.columns = inc.columns.map(lambda x: x.lower().replace(" ", "-"))
 	cum.index = cum.index.map(lambda x: np.round(x,1))
@@ -56,7 +58,7 @@ def postproc():
 
 	lst = [f for f in os.listdir(".") if f.lower().endswith(".lst")][0]
 	lst = GwtListBudget(lst)
-	cum,inc = lst.get_dataframes(diff=True,start_datetime=None)
+	inc,cum = lst.get_dataframes(diff=True,start_datetime=None)
 	cum.columns = cum.columns.map(lambda x: x.lower().replace(" ","-"))
 	inc.columns = inc.columns.map(lambda x: x.lower().replace(" ", "-"))
 	cum.index = cum.index.map(lambda x: np.round(x,1))
